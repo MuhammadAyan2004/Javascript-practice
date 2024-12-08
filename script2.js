501caac0318b3c04346a9e5ced9b2176045d5b7d
Muhammad AYAN <m101ayan92@gmail.com>
2024-12-08 13:14:33 +0500

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,14 @@
+(function (){
+
+let msg = "welcome to the world of programming";
+
+function greet(){
+    console.log(msg);
+}
+greet();
+})();
+
+setTimeout(function(){
+    let msg  = "nice to meet you buddy";
+    console.log(msg)
+},3000)
