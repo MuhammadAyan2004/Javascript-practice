c5823ba0403918c8825578a6b2c4ae08acc0d458
Muhammad AYAN <m101ayan92@gmail.com>
2024-12-14 01:34:18 +0500

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,14 +1,14 @@
-(function (){
+// (function (){
 
-let msg = "welcome to the world of programming";
+// let msg = "welcome to the world of programming";
 
-function greet(){
-    console.log(msg);
-}
-greet();
-})();
+// function greet(){
+//     console.log(msg);
+// }
+// greet();
+// })();
 
-setTimeout(function(){
-    let msg  = "nice to meet you buddy";
-    console.log(msg)
-},3000)
+// setTimeout(function(){
+//     let msg  = "nice to meet you buddy";
+//     console.log(msg)
+// },3000)
