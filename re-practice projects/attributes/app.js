8d218ad674d3cf499e2d3a77ac6a41c534fd3c22
Muhammad AYAN <m101ayan92@gmail.com>
2025-02-13 00:22:02 +0500

    Create app.js

---
+++ re-practice projects/attributes/app.js
@@ -0,0 +1,26 @@
+// let a = document.getElementById("first")
+
+// console.log(a.getAttribute("class"));
+// console.log(a.hasAttribute("class"));
+// console.log(a.setAttribute("class","my"));
+// console.log(a.attributes);
+// console.log(a.removeAttribute("class"));
+
+
+// insert element methods
+
+// let first = document.querySelector(".first")
+
+// const firstchild = document.createElement("div")
+// firstchild.classList.add("second")
+// firstchild.innerHTML = "i'm inside first "
+
+// first.appendChild(firstchild)
+// first.prepend(firstchild)
+// first.before(firstchild)
+// first.after(firstchild)
+
+// first.insertAdjacentHTML('beforebegin','<div class="test">beforebegin</div>')
+// first.insertAdjacentHTML('beforeend','<div class="test">beforeend</div>')
+// first.insertAdjacentHTML('afterbegin','<div class="test">afterbegin</div>')
+// first.insertAdjacentHTML('afterend','<div class="test">afterend</div>')
